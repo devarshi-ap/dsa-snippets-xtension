9564c1778d76d8393b7c2bceb80971bebe2a9a0c
Dev Patel <devarshi.ap@gmail.com>
2022-06-01 13:34:35 -0400

    first commit

---
+++ bin/Format.java
@@ -0,0 +1,14 @@
+package bin;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Format {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("bin.txt"));
+        while (scanner.hasNextLine()) {
+           String line = scanner.nextLine();
+           System.out.println("\"" + line + "\",");
+        }
+    }
+}
