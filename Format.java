ea41a0075ce60f039ec6779100bd4e3a39bfa2bc
Dev Patel <devarshi.ap@gmail.com>
2022-06-03 23:22:24 -0400

    add caesar cipher

--- Format.java
+++ Format.java
@@ -6,8 +6,8 @@ public class Format {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File("bin.txt"));
         while (scanner.hasNextLine()) {
-           String line = scanner.nextLine();
-           System.out.println("\"" + line + "\",");
+            String line = scanner.nextLine();
+            System.out.println("\"" + line + "\",");
         }
     }
 }
